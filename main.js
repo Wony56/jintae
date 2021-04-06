6625a13935a46ad0bb21cff8becf3dc4c43bf65b
Seokwon Lee <blackmonkey9256@gmail.com>
2021-04-06 20:31:33 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const bg = document.querySelector(".content");
-
-document.querySelector(".change-button").addEventListener("click", (event) => {
-  if (bg.style.background === "transparent") {
-    bg.style.background = "black";
-  } else {
-    bg.style.background = "transparent";
-  }
-});
